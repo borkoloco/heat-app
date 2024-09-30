403ca9aefee6fccae24b4c0fb1d33fabfba2abf6
borkoloco <santiagobosca@gmail.com>
2024-09-30 06:34:32 -0300

    deploy

--- heat-back/src/app.js
+++ heat-back/src/app.js
@@ -1,24 +1,3 @@
-const fs = require("fs");
-const path = require("path");
-
-// Retrieve the content of the secret from the environment variable
-const googleCredentials = process.env.GOOGLE_CREDENTIALS;
-
-if (googleCredentials) {
-  // Define the path where the JSON file will be written (inside the Fly.io instance)
-  const credentialsPath = path.join(__dirname, "foodexpress.json");
-
-  // Write the content to a JSON file
-  fs.writeFileSync(credentialsPath, googleCredentials);
-
-  // Update the GOOGLE_APPLICATION_CREDENTIALS environment variable to point to the new file
-  process.env.GOOGLE_APPLICATION_CREDENTIALS = credentialsPath;
-
-  console.log("Google credentials JSON file created at:", credentialsPath);
-} else {
-  console.error("GOOGLE_CREDENTIALS environment variable is missing.");
-}
-
 const express = require("express");
 const cors = require("cors");
 const sequelize = require("./config/database");
